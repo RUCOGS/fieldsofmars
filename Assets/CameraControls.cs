11fd4c42db941354d0d5c5f0d196e63becedf1a0
Gabriel <gabeajh@gmail.com>
2018-02-06 20:58:43 -0500

    Add Camera Controls and SelectionBox

---
+++ Assets/CameraControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControls : MonoBehaviour {
+
+	[SerializeField]
+    private int orthographicSizeMin;
+    [SerializeField]
+    private int orthographicSizeMax;
+    
+    void Update () {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0) {
+            Camera.main.orthographicSize--;
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") < 0) {
+            Camera.main.orthographicSize++;
+        }
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, orthographicSizeMin, orthographicSizeMax);
+
+        Camera.main.transform.Translate(new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")));
+    }
+}
---
+++ Assets/CreationBox.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationBox : MonoBehaviour {
+
+    private bool isSelecting = false;
+    private Vector2 selectionStart;
+    private Vector2 selectionEnd;
+    [SerializeField]
+    private Texture selectTexture;
+
+	void Update () {
+		if(!isSelecting && Input.GetMouseButtonDown(0)) {
+            isSelecting = true;
+            selectionStart = Input.mousePosition;
+        }
+
+        if(isSelecting && Input.GetMouseButtonUp(0)) {
+            isSelecting = false;
+            selectionEnd = Input.mousePosition;
+        }
+	}
+
+    void OnGUI() {
+        if(isSelecting) {
+            Rect selectRect = new Rect();
+            selectRect.x = selectionStart.x;
+            selectRect.y = Screen.height - selectionStart.y;
+            selectRect.width = Input.mousePosition.x - selectionStart.x;
+            selectRect.height = -1 * ((Screen.height - selectionStart.y) - (Screen.height - Input.mousePosition.y));
+            Debug.Log(selectRect);
+            GUI.DrawTexture(selectRect, selectTexture);
+        }
+    }
+}
